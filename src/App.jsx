33f19cd6ed59beea4fba7f1f84017166e01b4aa4
jeetisngh-621 <jeetsinghjeet68@gmail.com>
2024-05-06 10:18:04 +0530

    changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import Aboutus from './components/Aboutus'
 import Eco from './components/Eco'
 import Cards from './components/Cards'
 import Crypto from './components/Crypto'
+import LocomotiveScroll from 'locomotive-scroll';
+
+
 
 function App() {
+const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className='max-w-screen-2xl mx-auto min-h-[100vh] px-3 sm:px-16 text-white bg-[#03031b] overflow-hidden '>
       <Navbar/>
